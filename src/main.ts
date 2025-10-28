a71411ed9212690561dda7ed80a648804fb7ff49
maxolib <suchakree.sri@gmail.com>
2025-10-28 22:32:40 +0700

    update template

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,47 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from 'three/webgpu'
+import { color, texture, convertColorSpace } from 'three/tsl'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import Stats from 'three/addons/libs/stats.module.js'
-import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1.5
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10
+)
+camera.position.z = 1
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGPURenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+renderer.setAnimationLoop(animate)
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', function () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshNormalMaterial({ wireframe: true })
+const material = new THREE.NodeMaterial()
+// material.fragmentNode = color('blue')
+const tex = new THREE.TextureLoader().load('/grid_uv.jpg',)
+material.fragmentNode = convertColorSpace(texture(tex), THREE.SRGBColorSpace, THREE.LinearSRGBColorSpace)
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material)
+scene.add(mesh)
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
-
-const gui = new GUI()
-
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 20)
-cameraFolder.open()
+renderer.debug.getShaderAsync(scene, camera, mesh).then((e) => {
+  // console.log(e.vertexShader)
+  console.log(e.fragmentShader)
+})
 
 function animate() {
-  requestAnimationFrame(animate)
-
-  //cube.rotation.x += 0.01
-  //cube.rotation.y += 0.01
+  controls.update()
 
   renderer.render(scene, camera)
-
-  stats.update()
 }
-
-animate()
